c276dc5acbee18972b138752acf0825126447845
Abdel Atencio <abdelatencio@Abdels-MacBook-Pro.local>
2022-06-14 00:43:06 -0600

    style(general): link

--- src/load-script.ts
+++ src/load-script.ts
@@ -2,81 +2,81 @@ import { findScript, insertScriptElement, parseErrorMessage } from './utils';
 
 interface ONVONamespace {
   onvo : any;
-};
+}
 
 function getDefaultPromiseImplementation() {
-  if (typeof Promise === "undefined") {
-    throw new Error(
-      "Promise is undefined. To resolve the issue, use a Promise polyfill."
-    );
-  }
-  return Promise;
+    if (typeof Promise === "undefined") {
+        throw new Error(
+            "Promise is undefined. To resolve the issue, use a Promise polyfill."
+        );
+    }
+    return Promise;
 }
 
 function getONVOWindowNamespace(namespace: string): ONVONamespace {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return (window as any)[namespace];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (window as any)[namespace];
 }
 
 function validateArguments(options: unknown, PromisePonyfill?: unknown) {
-  if (typeof options !== "object" || options === null) {
-    throw new Error("Expected an options object.");
-  }
+    if (typeof options !== "object" || options === null) {
+        throw new Error("Expected an options object.");
+    }
 
-  if (
-    typeof PromisePonyfill !== "undefined" &&
+    if (
+        typeof PromisePonyfill !== "undefined" &&
     typeof PromisePonyfill !== "function"
-  ) {
-    throw new Error("Expected PromisePonyfill to be a function.");
-  }
+    ) {
+        throw new Error("Expected PromisePonyfill to be a function.");
+    }
 }
 
 export function loadScript(
-  PromisePonyfill: PromiseConstructor = getDefaultPromiseImplementation()
+    PromisePonyfill: PromiseConstructor = getDefaultPromiseImplementation()
 ): Promise<ONVONamespace | null> {
-  validateArguments({}, PromisePonyfill);
+    validateArguments({}, PromisePonyfill);
 
-  // resolve with null when running in Node
-  if (typeof window === "undefined" || typeof global.window === "undefined") return PromisePonyfill.resolve(null);
+    // resolve with null when running in Node
+    if (typeof window === "undefined" || typeof global.window === "undefined") return PromisePonyfill.resolve(null);
 
-  const url = "https://onvo-pay-widget.vercel.app/sdk.js";
-  const namespace = "onvo";
-  const existingWindowNamespace = getONVOWindowNamespace(namespace);
+    const url = "https://onvo-pay-widget.vercel.app/sdk.js";
+    const namespace = "onvo";
+    const existingWindowNamespace = getONVOWindowNamespace(namespace);
 
-  // resolve with the existing global onvo namespace when a script with the same params already exists
-  if (findScript(url) && existingWindowNamespace) {
-    return PromisePonyfill.resolve(existingWindowNamespace);
-  }
+    // resolve with the existing global onvo namespace when a script with the same params already exists
+    if (findScript(url) && existingWindowNamespace) {
+        return PromisePonyfill.resolve(existingWindowNamespace);
+    }
 
-  return new PromisePonyfill((resolve, reject) => {
+    return new PromisePonyfill((resolve, reject) => {
     // resolve with undefined when running in Node
-    if (typeof window === "undefined" || typeof global.window === "undefined") return resolve(null);
+        if (typeof window === "undefined" || typeof global.window === "undefined") return resolve(null);
 
-    insertScriptElement({
-      url,
-      attributes : {},
-      onSuccess : () => resolve(getONVOWindowNamespace(namespace)),
-      onError : () => {
-        const defaultError = new Error(
-          `The script "${url}" failed to load.`
-        );
+        insertScriptElement({
+            url,
+            attributes : {},
+            onSuccess : () => resolve(getONVOWindowNamespace(namespace)),
+            onError : () => {
+                const defaultError = new Error(
+                    `The script "${url}" failed to load.`
+                );
 
-        if (!window.fetch) {
-          return reject(defaultError);
-        }
-        // Fetch the error reason from the response body for validation errors
-        return fetch(url).then((response) => {
-          if (response.status === 200) {
-              reject(defaultError);
-          }
-          return response.text();
-        }).then((message) => {
-          const parseMessage = parseErrorMessage(message);
-          reject(new Error(parseMessage));
-        }).catch((err) => {
-          reject(err);
+                if (!window.fetch) {
+                    return reject(defaultError);
+                }
+                // Fetch the error reason from the response body for validation errors
+                return fetch(url).then((response) => {
+                    if (response.status === 200) {
+                        reject(defaultError);
+                    }
+                    return response.text();
+                }).then((message) => {
+                    const parseMessage = parseErrorMessage(message);
+                    reject(new Error(parseMessage));
+                }).catch((err) => {
+                    reject(err);
+                });
+            },
         });
-      },
     });
-  });
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,53 +1,53 @@
 type StringMap = Record<string, string>;
 
 export function findScript(url: string): HTMLScriptElement | null {
-  const currentScript = document.querySelector<HTMLScriptElement>(
-    `script[src="${url}"]`
-  );
-  if (currentScript === null) return null;
+    const currentScript = document.querySelector<HTMLScriptElement>(
+        `script[src="${url}"]`
+    );
+    if (currentScript === null) return null;
 
-  const nextScript = createScriptElement(url);
+    const nextScript = createScriptElement(url);
 
-  // ignore the data-uid-auto attribute that gets auto-assigned to every script tag
-  const currentScriptClone = currentScript.cloneNode() as HTMLScriptElement;
-  delete currentScriptClone.dataset.uidAuto;
+    // ignore the data-uid-auto attribute that gets auto-assigned to every script tag
+    const currentScriptClone = currentScript.cloneNode() as HTMLScriptElement;
+    delete currentScriptClone.dataset.uidAuto;
 
-  // check if the new script has the same number of data attributes
-  if (
-    Object.keys(currentScriptClone.dataset).length !==
+    // check if the new script has the same number of data attributes
+    if (
+        Object.keys(currentScriptClone.dataset).length !==
     Object.keys(nextScript.dataset).length
-  ) {
-    return null;
-  }
-
-  let isExactMatch = true;
-
-  // check if the data attribute values are the same
-  Object.keys(currentScriptClone.dataset).forEach((key) => {
-    if (currentScriptClone.dataset[key] !== nextScript.dataset[key]) {
-      isExactMatch = false;
+    ) {
+        return null;
     }
-  });
 
-  return isExactMatch ? currentScript : null;
+    let isExactMatch = true;
+
+    // check if the data attribute values are the same
+    Object.keys(currentScriptClone.dataset).forEach((key) => {
+        if (currentScriptClone.dataset[key] !== nextScript.dataset[key]) {
+            isExactMatch = false;
+        }
+    });
+
+    return isExactMatch ? currentScript : null;
 }
 
 function createScriptElement(
-  url: string,
-  attributes: StringMap = {}
+    url: string,
+    attributes: StringMap = {}
 ): HTMLScriptElement {
-  const newScript: HTMLScriptElement = document.createElement("script");
-  newScript.src = url;
+    const newScript: HTMLScriptElement = document.createElement("script");
+    newScript.src = url;
 
-  Object.keys(attributes).forEach((key) => {
-    newScript.setAttribute(key, attributes[key]);
+    Object.keys(attributes).forEach((key) => {
+        newScript.setAttribute(key, attributes[key]);
 
-    if (key === "data-csp-nonce") {
-      newScript.setAttribute("nonce", attributes["data-csp-nonce"]);
-    }
-  });
+        if (key === "data-csp-nonce") {
+            newScript.setAttribute("nonce", attributes["data-csp-nonce"]);
+        }
+    });
 
-  return newScript;
+    return newScript;
 }
 
 export interface ScriptElement {
@@ -58,17 +58,17 @@ export interface ScriptElement {
 }
 
 export function insertScriptElement({ url, attributes, onSuccess, onError }: ScriptElement): void {
-  const newScript = createScriptElement(url, attributes);
-  newScript.onerror = onError;
-  newScript.onload = onSuccess;
+    const newScript = createScriptElement(url, attributes);
+    newScript.onerror = onError;
+    newScript.onload = onSuccess;
 
-  document.head.insertBefore(newScript, document.head.firstElementChild);
+    document.head.insertBefore(newScript, document.head.firstElementChild);
 }
 
 export function parseErrorMessage(message: string): string {
-  const originalErrorText = message.split("/* Original Error:")[1];
+    const originalErrorText = message.split("/* Original Error:")[1];
 
-  return originalErrorText
-    ? originalErrorText.replace(/\n/g, "").replace("*/", "").trim()
-    : message;
+    return originalErrorText
+        ? originalErrorText.replace(/\n/g, "").replace("*/", "").trim()
+        : message;
 }
