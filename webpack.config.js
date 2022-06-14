7db943e53376be9590e93b34e5c1aca5676febe2
Abdel Atencio <abdelatencio@Abdels-MacBook-Pro.local>
2022-06-14 00:45:48 -0600

    style(general): lint fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 import path from 'path';
 
 module.exports = {
-	entry: './src/index.ts',
-	mode   : 'production',
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename : 'onvo.min.js',
-		library  : {
-			name : 'onvo',
-			type : 'umd',
-		},
-	},
+    entry: './src/index.ts',
+    mode   : 'production',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename : 'onvo.min.js',
+        library  : {
+            name : 'onvo',
+            type : 'umd',
+        },
+    },
 };
 
